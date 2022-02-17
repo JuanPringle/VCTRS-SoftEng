207f8553a6243473bfe498e530fd6a23af7ef458
Sajid2001 <sajidsyed201@gmail.com>
2022-02-17 17:19:33 -0500

    GUI change
    changed to a dropdown menu, button does not work yet.

--- scr/GUI.java
+++ scr/GUI.java
@@ -1,39 +1,68 @@
-import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.FileNotFoundException;
-public class GUI implements ActionListener{
-	private JFrame frame;
-	private JPanel panel;
-	private JLabel labelA;
-	private JLabel labelB;
-	private JLabel labelC;
-	private JButton buttonA;
+
+public class GUI extends JFrame implements ActionListener{
+	private JPanel topPanel;
+	private JPanel bottomPanel;
+
+	private JTextField textField;
+	private JButton button;
+	private JComboBox<String> comboBox;
 	
 	
 	public GUI(){
-		frame= new JFrame();
-		panel = new JPanel();
-		labelA = new JLabel("Owner");
-		labelB = new JLabel("UwU");
-		labelC = new JLabel("Client");
-		panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-		panel.setLayout(new GridLayout(1,3));
-		panel.add(labelA);
-		panel.add(labelB);
-		panel.add(labelC);
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Cars");
-		frame.pack();
-		frame.setVisible(true);
+
+		
+		//panels + layout
+		topPanel = new JPanel();
+		topPanel.setBounds(0,0,750,150);
+		topPanel.setBackground(Color.red);
+
+		bottomPanel = new JPanel();
+		bottomPanel.setBounds(0,150,750, 600);
+		bottomPanel.setBackground(Color.GRAY);
+
+		//Dropdown menu
+		String[] options = {"Owner", "Client"};
+		comboBox = new JComboBox<String>(options);
+		comboBox.addActionListener(this);
+
+		//text fields
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(250,40));
+
+		JButton button = new JButton("Submit");
+		button.addActionListener(this);
+
+
+		
+		topPanel.add(comboBox, BorderLayout.CENTER);
+		bottomPanel.add(textField);
+		bottomPanel.add(button);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Vehicual Cloud Program");
+		this.setLayout(null);
+		this.add(topPanel);
+		this.add(bottomPanel);
+		this.pack();
+		this.setSize(750,750);
+		this.setVisible(true);
+		
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (e.getSource() == comboBox) {
+			System.out.println(comboBox.getSelectedItem());
+		}
+
+		if (e.getSource() == button){
+			System.out.println("Hello World");
+		}
 		
 	}
 	public static void main(String[] args) throws FileNotFoundException {
