f82d11bcc94176e674790de6c48a067a106406b9
Sajid2001 <60523377+Sajid2001@users.noreply.github.com>
2022-02-19 16:06:57 -0500

    Gui improvements

--- scr/GUI.java
+++ scr/GUI.java
@@ -1,42 +1,120 @@
-import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.FileNotFoundException;
-public class GUI implements ActionListener{
-	private JFrame frame;
-	private JPanel panel;
-	private JLabel labelA;
-	private JLabel labelB;
-	private JLabel labelC;
-	private JButton buttonA;
+public class GUI extends JFrame implements ActionListener{
+
+	// instance variables
+	
+	private JPanel dropdownPanel, textInputPanel, buttonInputPanel, textOutputPanel;
+	private JTextField boxOne, boxTwo, boxThree;
+	private JTextArea output;
+	private JLabel labelA, labelB, labelC;
+	private JComboBox combo;
+	private JButton button;
+	
+
 	
 	
 	public GUI(){
-		frame= new JFrame();
-		panel = new JPanel();
-		labelA = new JLabel("Owner");
-		labelB = new JLabel("UwU");
-		labelC = new JLabel("Client");
-		panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-		panel.setLayout(new GridLayout(1,3));
-		panel.add(labelA);
-		panel.add(labelB);
-		panel.add(labelC);
-		frame.add(panel, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Cars");
-		frame.pack();
-		frame.setVisible(true);
+
+		//setting the layout, size, and title of the GUI
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		this.setTitle("Cars");
+		this.setSize(600,800);
+
+		//instantaited panels in the GUI
+		dropdownPanel = new JPanel();
+
+		textInputPanel = new JPanel();
+		textInputPanel.setLayout(new GridLayout(3,2));
+
+		buttonInputPanel = new JPanel();
+
+
+		textOutputPanel = new JPanel();
+		textOutputPanel.setLayout(new BorderLayout());
+		
+
+		//combo box instantiation, goes into the dropdown Panel
+		String[] choices = {" ","Owner", "Client"};
+		combo = new JComboBox(choices);
+		combo.addActionListener(this);
+
+		//all the text boxes and their labels, goes into the text input panel
+		boxOne = new JTextField();
+		boxOne.setPreferredSize(new Dimension(250,40));
+		boxTwo = new JTextField();
+		boxTwo.setPreferredSize(new Dimension(250,40));
+		boxThree = new JTextField();
+		boxThree.setPreferredSize(new Dimension(250,40));
+
+		labelA = new JLabel("Nothing Selected", SwingConstants.CENTER);
+		labelB = new JLabel("Nothing Selected", SwingConstants.CENTER);
+		labelC = new JLabel("Nothing Selected", SwingConstants.CENTER);
+
+		//text area at the lowest panel of the JFrame
+		//not editable, plan to add feature that outputs what will be input by the user
+		output = new JTextArea();
+		output.setEditable(false);
+		output.setMargin( new Insets(10,10,10,10) );
+
+
+		//button to submit what was input, goes into the button input panel
+		button = new JButton("Submit");
+		button.addActionListener(this);
+
+		//adds all elements to their respective panels
+		dropdownPanel.add(combo);
+		textInputPanel.add(labelA);
+		textInputPanel.add(boxOne);
+		textInputPanel.add(labelB);
+		textInputPanel.add(boxTwo);
+		textInputPanel.add(labelC);
+		textInputPanel.add(boxThree);
+		buttonInputPanel.add(button);
+		textOutputPanel.add(output);
+		
+		//adds each panel to the main JFrame
+		this.add(dropdownPanel);
+		this.add(textInputPanel);
+		this.add(buttonInputPanel);
+		this.add(textOutputPanel);
+
+
+		this.pack();
+		this.setVisible(true);
 		
 	}
 
+	//action listener method
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.getSource() == combo)
+		{
+			//sees which choice in the dropdown gets selected 
+			//and changes the labels accordingly
+			if (combo.getSelectedItem().equals("Owner"))
+			{
+				labelA.setText("Owner ID");
+				labelB.setText("Vehicle Info");
+				labelC.setText("Residency Time");
+			}
+			else if (combo.getSelectedItem().equals("Client"))
+			{
+				labelA.setText("Client ID");
+				labelB.setText("Approximate Time");
+				labelC.setText("Job Deadline");
+			}
+		}
+
+		//this area needs more work
+		//when the button is pressed, the bottom text area should output what was input. 
+		//can delete this feature if not necessary
+		if(e.getSource() == button)
+		{
+			output.setText("Information submitted!\n" + boxOne.getText() + "\n" + boxTwo.getText() + "\n" + boxThree.getText() + "\n" );
+		}
 	}
-	public static void main(String[] args) throws FileNotFoundException {
-		new GUI();
-	}
+
 }
