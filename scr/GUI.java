2ce714d8acca4a4f4cdb43770b3532156390ab05
DavidVettuchirayil <81113579+DavidVettuchirayil@users.noreply.github.com>
2022-02-20 17:59:00 -0500

    Implemented Text to File Process

--- scr/GUI.java
+++ scr/GUI.java
@@ -1,29 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class GUI extends JFrame implements ActionListener{
+import java.io.*;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+public class GUI extends JFrame implements ActionListener {
 
-	// instance variables
-	
+	// Instance variables
 	private JPanel dropdownPanel, textInputPanel, buttonInputPanel, textOutputPanel;
 	private JTextField boxOne, boxTwo, boxThree;
 	private JTextArea output;
 	private JLabel labelA, labelB, labelC;
 	private JComboBox combo;
 	private JButton button;
+	private File ownerFile = new File("Owner.txt");
+	private File clientFile = new File("Client.txt");	
 	
+	public GUI() {
 
-	
-	
-	public GUI(){
-
-		//setting the layout, size, and title of the GUI
+		//Setting the layout, size, and title of the GUI
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		this.setTitle("Cars");
 		this.setSize(600,800);
 
-		//instantaited panels in the GUI
+		//Instantaited panels in the GUI
 		dropdownPanel = new JPanel();
 
 		textInputPanel = new JPanel();
@@ -31,17 +32,15 @@ public class GUI extends JFrame implements ActionListener{
 
 		buttonInputPanel = new JPanel();
 
-
 		textOutputPanel = new JPanel();
 		textOutputPanel.setLayout(new BorderLayout());
 		
-
-		//combo box instantiation, goes into the dropdown Panel
+		//Combo box instantiation, goes into the dropdown Panel
 		String[] choices = {" ","Owner", "Client"};
 		combo = new JComboBox(choices);
 		combo.addActionListener(this);
 
-		//all the text boxes and their labels, goes into the text input panel
+		//All the text boxes and their labels, goes into the text input panel
 		boxOne = new JTextField();
 		boxOne.setPreferredSize(new Dimension(250,40));
 		boxTwo = new JTextField();
@@ -53,18 +52,17 @@ public class GUI extends JFrame implements ActionListener{
 		labelB = new JLabel("Nothing Selected", SwingConstants.CENTER);
 		labelC = new JLabel("Nothing Selected", SwingConstants.CENTER);
 
-		//text area at the lowest panel of the JFrame
-		//not editable, plan to add feature that outputs what will be input by the user
+		//Text area at the lowest panel of the JFrame
+		//Not editable, plan to add feature that outputs what will be input by the user
 		output = new JTextArea();
 		output.setEditable(false);
 		output.setMargin( new Insets(10,10,10,10) );
 
-
-		//button to submit what was input, goes into the button input panel
+		//Button to submit what was input, goes into the button input panel
 		button = new JButton("Submit");
 		button.addActionListener(this);
 
-		//adds all elements to their respective panels
+		//Adds all elements to their respective panels
 		dropdownPanel.add(combo);
 		textInputPanel.add(labelA);
 		textInputPanel.add(boxOne);
@@ -75,25 +73,23 @@ public class GUI extends JFrame implements ActionListener{
 		buttonInputPanel.add(button);
 		textOutputPanel.add(output);
 		
-		//adds each panel to the main JFrame
+		//Adds each panel to the main JFrame
 		this.add(dropdownPanel);
 		this.add(textInputPanel);
 		this.add(buttonInputPanel);
 		this.add(textOutputPanel);
 
-
 		this.pack();
-		this.setVisible(true);
-		
+		this.setVisible(true);		
 	}
 
-	//action listener method
+	//Action listener method
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == combo)
 		{
-			//sees which choice in the dropdown gets selected 
-			//and changes the labels accordingly
+			//Sees which choice in the dropdown gets selected 
+			//And changes the labels accordingly
 			if (combo.getSelectedItem().equals("Owner"))
 			{
 				labelA.setText("Owner ID");
@@ -107,14 +103,44 @@ public class GUI extends JFrame implements ActionListener{
 				labelC.setText("Job Deadline");
 			}
 		}
-
-		//this area needs more work
+		
 		//when the button is pressed, the bottom text area should output what was input. 
-		//can delete this feature if not necessary
 		if(e.getSource() == button)
 		{
+			try 
+			{
+				fileProcess();
+			} 
+			catch (IOException e1) 
+			{
+				e1.printStackTrace();
+			}
 			output.setText("Information submitted!\n" + boxOne.getText() + "\n" + boxTwo.getText() + "\n" + boxThree.getText() + "\n" );
 		}
 	}
-
+	
+	public void fileProcess() throws IOException {
+			if(combo.getSelectedItem().equals("Owner")) {
+				BufferedWriter ownerWriter = new BufferedWriter(new FileWriter(ownerFile,true));
+				ownerWriter.write(boxOne.getText());
+				ownerWriter.newLine();
+				ownerWriter.write(boxTwo.getText());
+				ownerWriter.newLine();
+				ownerWriter.write(boxThree.getText());
+				ownerWriter.newLine();
+				ownerWriter.newLine();
+				ownerWriter.close();
+			}
+			else if (combo.getSelectedItem().equals("Client")) {
+				BufferedWriter clientWriter = new BufferedWriter(new FileWriter(clientFile,true));
+				clientWriter.write(boxOne.getText());
+				clientWriter.newLine();
+				clientWriter.write(boxTwo.getText());
+				clientWriter.newLine();
+				clientWriter.write(boxThree.getText());
+				clientWriter.newLine();
+				clientWriter.newLine();
+				clientWriter.close();
+			}
+		}
 }
