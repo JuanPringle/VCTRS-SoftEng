a5c9c035db04e93c34d3c04e06fdb6bf1b5996b3
tosinalao <79558547+tosinalao@users.noreply.github.com>
2022-03-21 21:16:13 -0400

    Update Vehicle.java

--- scr/Vehicle.java
+++ scr/Vehicle.java
@@ -1,4 +1,17 @@
+import java.util.Date;
 
 public class Vehicle {
+	private Date Schedule;
+	private String info;
 
+	public Date getSchedule() {
+		return Schedule;
+	}
+	public void arrive () {
+		
+	}
+	
+	public void depart() {
+		
+	}
 }
