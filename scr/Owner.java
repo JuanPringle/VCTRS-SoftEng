242da5a8c1c859da800e9a2adc9aacb845fdfdba
JuanPringle <83386505+JuanPringle@users.noreply.github.com>
2022-03-22 21:21:30 -0400

    Update Owner.java

--- scr/Owner.java
+++ scr/Owner.java
@@ -1,19 +1,24 @@
-public class Owner {
+public class VehicleOwner extends User {
 	private int OwnerID;
-	private String VehicleInfo;
+	private String[] VehicleInfo;
 	private String ResidencyTime;
-	public Owner(int OwnerID, String VehicleInfo, String ResidencyTime){
-		this.OwnerID=OwnerID;
+	private Vehicle[] VehiclesOwned;
+	public VehicleOwner(int OwnerID, String VehicleInfo[], String ResidencyTime, Vehicle[] VehiclesOwned){
+		super(OwnerID);
 		this.VehicleInfo=VehicleInfo;
 		this.ResidencyTime=ResidencyTime;
+		this.VehiclesOwned=VehiclesOwned;
 	}
 	public int getOwnerID(){
 		return OwnerID;
 	}
-	public String getVehicleInfo(){
+	public String[] getVehicleInfo(){
 		return VehicleInfo;
 	}
 	public String getResidencyTime(){
 		return ResidencyTime;
+	}
+	public void rent(Vehicle vehicle) {
+		
 	}
 }
